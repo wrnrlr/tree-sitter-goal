69dd1d2696b9393072b080d45dd7119b9928e8cf
wrnrlr <werner@alman.ax>
2025-10-05 06:43:58 +0200

    fix array

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 A tree-sitter grammer for the Goal array programming language.
 
 ## Dev Tips
-- Run generate, make and parse: `tree-sitter generate && make clean && make && tree-sitter parse test/_.goal`.
-- Execute goal program with `g` command: `g <file.goal>`.
+- Run generate, make and parse: `tree-sitter generate && make clean && make && tree-sitter parse test/_.goal && echo "OK" || echo "FAIL"`.
+- Execute goal program with `g` command: `g <file.goal> && echo "OK" || echo "FAIL"`.
--- grammar.js
+++ grammar.js
@@ -1,83 +1,63 @@
-const decimal = /\d(_?\d)*/
-const name = /[a-zA-Z_π][a-zA-Z0-9_π]*/
+const name = /[\W_π][\w_π]*/
+const sep = /[\n;]/
+const comment = token(/\/[^\n]*/)
 
 module.exports = grammar({
   name: 'goal',
 
   conflicts: $ => [
+    [$.diadic, $.monadic],
   ],
 
   extras: $ => [/\s/],
 
   rules: {
     // The root rule for the grammar: statements separated by separators
-    program: $ => seq($.statement, repeat(seq($.sep, $.statement)), optional($.sep)),
+    program: $ => repeat($.statement),
 
-    statement: $ => choice($.assignment, $.expression, $.comment),
+    statement: $ => choice($.assign, $.expr, comment),
 
-    expression: $ => $.binary_expr,
+    expr: $ => $.binary_expr,
 
-    binary_expr: $ => prec.left(seq($.monadic_expr, repeat(seq($.diadic, $.monadic_expr)))),
+    binary_expr: $ => prec.left(seq($.monadic_expr, repeat(choice(seq($.diadic, $.monadic_expr), $.monadic_expr)))),
 
     monadic_expr: $ => seq(repeat($.monadic), $.primary),
 
-    assignment: $ => prec(1, seq(name, ':', $.expression)),
+    assign: $ => prec(1, seq(name, ':', $.expr)),
 
-    primary: $ => choice(
-      $.atom,
-      $.group
-    ),
+    primary: $ => choice( $.array, $.atom, $.group ),
 
     atom: $ => choice(
       $.number,
       name,
       $.string,
-      $.array,
-      $.unparenthesized_array,
       $.lambda,
       $.cond,
-      $.colon
     ),
 
-    colon: _ => ':',
+    array: $ => prec(3, seq('(', $.body, ')')),
 
-        array: $ => prec(2, seq('(', optional($.array_body), ')')),
+    group: $ => prec(2, seq('(', $.expr, ')')),
 
-    group: $ => prec(1, seq('(', $.expression, ')')),
+    body: $ => repeat1($.expr),
 
-    array_body: $ => seq(
-      $.expression,
-      repeat(seq($.sep, $.expression))
-    ),
-
-    diadic: _ => choice(':', '+', '-', '*', '%', '!', '&', '|', '<', '>', '=', '~', ',', '^', '#', '_', '$', '?', '@', '.', "'"),
+    diadic: _ => choice(':', '+', '-', '*', '%', '!', '&', '|', '<', '>', '=', '~', ',', '^', '#', '_', '$', '?', '@', '.', "'", ';'),
 
     monadic: _ => choice('+', '-', '!', '~', 'abs'),
 
     // Number literals supporting the specified formats
     number: $ => token(choice(
-        /0x[\da-f]+/i,
-        /0[bB][01](?:_?[01])*/,
-        /(?:\d+\.\d+(?:e[+-]?\d+)?|\d+(?:e[+-]?\d+)?)/i,
-      )
-    ),
-
-    // String literals
-    string: _ => token(seq('"', repeat(choice(/[^"\\]/, seq('\\', /./))), '"')),
-
-    unparenthesized_array: $ => prec.left(-1, seq(
-      $.primary,
-      repeat1($.primary)
+      /0x[\da-f]+/i,
+      /0[bB][01](?:_?[01])*/,
+      /(?:\d+\.\d+(?:e[+-]?\d+)?|\d+(?:e[+-]?\d+)?)/i,
     )),
 
-    lambda: $ => seq('{', optional(seq($.args, optional(';'))), optional($.expression), '}'),
+    string: _ => token(seq('"', repeat(choice(/[^"\\]/, seq('\\', /./))), '"')),
+
+    lambda: $ => seq('{', optional(seq($.args, optional(';'))), optional($.expr), '}'),
 
     args: _ => seq('[', name, repeat(seq(';', name)), ']'),
 
-    cond: $ => seq('?[', $.expression, $.sep, $.expression, $.sep, $.expression, ']'),
-
-    sep: _ => choice('\n', ';'),
-
-    comment: _ => token(/\/[^\n]*/),
+    cond: $ => seq('?[', $.expr, ';', $.expr, ';', $.expr, ']'),
   }
 });
