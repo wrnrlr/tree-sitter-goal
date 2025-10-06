a40df5c63043f9ae6a3d122e5505c9b6303fa738
wrnrlr <werner@alman.ax>
2025-10-06 20:29:15 +0200

    add strand and simple noun

--- README.md
+++ README.md
@@ -6,12 +6,24 @@ A tree-sitter grammer for the Goal array programming language.
 - Test parsing of `.goal` file with clean build:
   ```
   tree-sitter generate && \
-    make clean && make && \
-    tree-sitter parse <file.goal> && echo "OK" || echo "ERROR"
+    make && \
+    gtimeout 30s tree-sitter parse test/*.goal && \
+    echo "OK" || \
+    if [ $? -eq 124 ]; then
+      echo "TIMEOUT"
+    else
+      echo "ERROR"
+    fi
   ```
 - Run file with goal interpreter `g` as authoritative source of valid syntax:
   ```
-  g <file.goal> && echo "OK" || echo "ERROR"
+
+    /Users/werner/Code/goal/goal -e "1 + 1"
+
+  /Users/werner/Code/goal/goal <<EOF && echo "OK" || echo "ERROR"
+  1+1
+
+  EOF
   ```
 
 ## Open Questions
--- grammar.js
+++ grammar.js
@@ -1,4 +1,8 @@
 /*
+Expression is a sequence of noun/verb chains, possibly grouped, possibly separated by semicolons.
+Verbs may be atomic or modified by adverbs.
+Nouns may be atoms, grouped expressions, blocks, or indexed terms.
+
 Be careful with whitespace, it sometimes affect hopw things are parsed.
 Arrays for example
 
@@ -7,78 +11,79 @@ Array of 3 numbers
 1 2 3
 ```
 
-binairy/dyadic operator Array of 3 numbers
+binairy/dyadic operator Array of 3 numbers plus 3 other numbers
 ```goal
 1 2 3 + 4 5 6
 ```
 
-
+An group can even be spread out over multiple lines
+```goal
+a: (1 2
+3 4)
+```
 */
 
 export default grammar({
   name: 'goal',
-  supertypes: $ => [$.e, $.N],
-  conflicts: $ => [],
+  supertypes: $ => [$.atom],
+  conflicts: $ => [[$.n, $.strand], [$.strand], [$.atom, $.args]],
+  extras: $ => [/[\t ]+/],
   rules: {
+    source_file: $ => optional($.E),
     // E: E ; E | e
-    E: $ => repeatSep(choice(
-      $.comment,
-      repeatSep($.e, ';')
-    ), '\n'),
-    // e: n v e | te | ε
-    e: $ => choice(
-      $.nve,
-      $.te,
-      $.empty // I am not sure about this, the grammer says this should be empty, not sure they mean "nothing" on an lexical level, or the semantic construction "empty array with token $.empty".
+    E: $ => seq(
+      optional(choice('\n', ';')),
+      choice($.comment, $.e),
+      repeat(seq(choice('\n', ';'), choice($.comment, $.e))),
+      optional(choice('\n', ';'))
     ),
-    nve: $ => seq($.n, $.v, $.e),
-    te: $ => seq($.t, $.e),
+    e: $ => choice($.nve, $.te, seq('[', optional($.E), ']'), $.t),
+    nve: $ => prec.right(seq($.n, $.v, optional($.e))),
+    te: $ => prec.right(seq($.t, $.e)),
     // t : n | v
     t: $ => prec.right(choice(
       $.n,
       $.v
     )),
     // t A | V
-    v: $ => choice(
-      seq($.t, /\s+/, $.A),
-      $.V
-    ),
-    // n : t [ E ] | ( E ) | { E } | N
+    v: $ => prec.left(choice(
+      $.V,
+      seq($.v, token.immediate(choice('/', '\\', "'"))),
+      $.A
+    )),
     n: $ => choice(
-      // seq($.t, '[', $.E, ']'),
-      // seq('?[', $.E, ']'),
-      // seq(/\(/, $.E, /\)/),
-      // seq('{', optional($.args), $.E, '}'),
-      $.N
+      seq($.t, token.immediate('['), optional($.E), ']'),
+      $.strand,
+      $.simple_noun
+    ),
+    simple_noun: $ => choice(
+      $.atom,
+      seq('(', optional($.E), ')'),
+      seq('{', optional($.args), optional($.E), '}')
+    ),
+    strand: $ => seq(
+      $.simple_noun,
+      repeat1($.simple_noun)
     ),
     V: $ => /[:+\-*%!&|<>=~,^#_$?@.;]/,
     A: $ => token(choice('/', '\\', "'")),
-    N: $ => choice(
-      $.empty, $.nil, $.infinity,
+    atom: $ => choice(
+      $.nil, $.infinity,
       $.int, $.decimal, $.hexadecimal, $.standard_form,
       $.name,
       $.string,
     ),
-    empty: _ => /\(\s*\)/,
+
     nil: _ => /0n/i,
     infinity: _ => /0w/i,
-    diadic: _ => /[:+\-*%!&|<>=~,^#_$?@.;]|::/,
-    monadic: _ => token(choice(':', '+', '-', '!', '~', '$', '|', '=', '*', '%', '@', '?', 'abs')),
+
     string: _ => seq('"', repeat(choice(/[^"\\]/, seq('\\', /./))), '"'),
     args: $ => seq('[', $.name, repeat(seq(';', $.name)), ']'),
-    int: _ => /\d+/,
-    decimal: _ => /\d+\.\d+/,
-    hexadecimal: _ => /0x[\da-f]+/i,
-    standard_form: _ => /\d+e-?\d+/,
+    int: _ => token(/-?\d+/),
+    decimal: _ => token(/-?\d+\.\d+/),
+    hexadecimal: _ => token(/-?0x[\da-f]+/i),
+    standard_form: _ => token(/-?\d+e-?\d+/),
     name: _ => /[a-zA-Z_π][\w_π]*/,
     comment: _ => /\/[^\n]*/,
   }
 });
-
-function repeatSep(item, separator, trailing = true) {
-  return seq(
-    item,
-    repeat(seq(separator, item)),
-    trailing ? optional(separator) : ''
-  );
-}
