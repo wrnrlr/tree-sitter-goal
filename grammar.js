c3a2476b36f316aa31d35a76cd3b178df5113c2d
wrnrlr <werner@alman.ax>
2025-10-06 23:11:02 +0200

    time literal

--- grammar.js
+++ grammar.js
@@ -1,11 +1,13 @@
 export default grammar({
   name: 'goal',
-  supertypes: $ => [$.atom],
+  supertypes: $ => [$.atom, $.N, $.t],
   conflicts: $ => [[$.n, $.strand], [$.strand], [$.atom, $.args], [$.e, $.te]],
   extras: $ => [/[\t ]+/],
   rules: {
+    source_file: $ => optional($.E),
     // E: E ; E | e
-    E: $ => optional(repeat1(choice($.comment, $.e, token('\n')))),
+    E: $ => repeat1(choice($.line_comment, $.e, token('\n'))),
+    // e : n v e | t e | ε
     e: $ => choice($.nve, $.te, seq('[', optional($.E), ']'), $.t),
     nve: $ => prec.right(seq($.n, $.v, optional($.e))),
     te: $ => prec.right(seq($.t, $.e)),
@@ -13,29 +15,27 @@ export default grammar({
     t: $ => prec.right(choice( $.n, $.v )),
     // t A | V
     v: $ => prec.left(choice(
-      $.V, $.A,
+      $.V,
       seq($.v, token.immediate(choice('/', '\\', "'"))),
+      $.A
     )),
     n: $ => choice(
-      $.strand, $.simple_noun,
-      seq($.t, token.immediate('['), optional($.E), ']')
+      seq($.t, token.immediate('['), optional($.E), ']'),
+      $.strand,
+      $.N
     ),
-    simple_noun: $ => choice(
-      $.atom,
-      seq('(', optional($.E), ')'),
-      seq('{', optional($.args), optional($.E), '}')
-    ),
-    strand: $ => seq(
-      $.simple_noun,
-      repeat1($.simple_noun)
-    ),
+    N: $ => choice( $.atom, $.group, $.lambda ),
+    strand: $ => seq( $.N, repeat1($.N) ),
     V: $ => /[:+\-*%!&|<>=~,^#_$?@.;]/,
     A: $ => token(choice('/', '\\', "'")),
     atom: $ => choice(
       $.nil, $.infinity,
       $.int, $.decimal, $.hexadecimal, $.standard_form,
+      $.time,
       $.name, $.string
     ),
+    group: $ => seq('(', optional($.E), ')'),
+    lambda: $ => seq('{', optional($.args), optional($.E), '}'),
     nil: _ => /0n/i,
     infinity: _ => /0w/i,
     string: _ => seq('"', repeat(choice(/[^"\\]/, seq('\\', /./))), '"'),
@@ -45,6 +45,9 @@ export default grammar({
     hexadecimal: _ => token(/-?0x[\da-f]+/i),
     standard_form: _ => token(/-?\d+e[+-]?\d+/),
     name: _ => /[a-zA-Z_π][\w_π]*/,
-    comment: _ => /\/[^\n]*/,
+    time: _ => token(/-?(\d+(?:\.\d+)?(?:e[+-]?\d+)?[hms])+/),
+    line_comment: _ => /\/[^\n]*/,
+    // block_comment: null, // start with a single slash on a line and end with only a single bashslash on line
+    // inline_comment: null, // comment that starts after whitespace and a slash
   }
 });
